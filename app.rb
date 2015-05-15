1a2f07df10092b622bb45f7174e4d1bda431524e
Toby <Alden@Tobys-MacBook-Pro.local>
2015-05-15 10:02:48 -0700

    Add integration spec for path to add band. Pass spec.

--- app.rb
+++ app.rb
@@ -6,3 +6,13 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/bands') do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+post('/bands') do
+  @new_band = Band.create({:name => params.fetch("band_name")})
+  redirect('/bands')
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,3 +6,21 @@ describe("The path to the home page", {:type => :feature}) do
     expect(page).to(have_content("Band Tracker"))
   end
 end
+
+describe("The path to the bands page", {:type => :feature}) do
+  it("displays a list of all the bands in the database.") do
+    visit('/')
+    click_button('bands')
+    expect(page).to(have_content("List of Bands:"))
+  end
+end
+
+describe("The path to add a band", {:type => :feature}) do
+  it("displays a form that adds a band to the database when submitted.") do
+    visit('/')
+    click_button('bands')
+    fill_in('band_name', :with => 'Boris')
+    click_button('add_band')
+    expect(page).to(have_content("Boris"))
+  end
+end
